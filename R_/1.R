1fb41251c665060fd00168ce63aae2b3b0735933
ADHIKARI KRISHNA <45994360+krishdb38@users.noreply.github.com>
2020-09-07 22:37:57 +0900

    z_score

---
+++ R_/1.R
@@ -0,0 +1,47 @@
+x = c(10,5,3,6)
+x
+y = c(x,0.55, x,x)
+y
+round(y)
+abs(y)
+asin(y)
+a = 10
+sin(a)
+exp(10)
+sqrt(4)
+log(a)
+sqrt(a) + 2
+index= 1:20
+index
+u = seq(from =-3, to = 100)
+u
+seq(1:100)
+1:10
+rep?
+
+;
+help(rep)
+x = rnorm(10)
+x
+y  = runif(10,4,7)
+y
+x = 1:8
+dim(x)
+x = matrix(1:8,2,4)
+x
+
+
+
+x
+dim(x) = c(2,4)
+c(1,3)
+1,3
+paste("Hello","hello")
+
+
+
+
+cbind(c(1,2,3),c(4,5,6))
+rbind(c(1,2,3),c(4,5,6))
+
+x = matrix(1:28, ncol = 7)
